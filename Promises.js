684035d8665bbfb43c4b7194b2c3e467160c5f5c
celikn <naimecelik@gmail.com>
2021-09-10 11:12:05 +0300

    Update Promises.js
    Promise example

--- Promises.js
+++ Promises.js
@@ -29,3 +29,36 @@
                 //(error) => console.log(error)
             ).cathc((error)=> console.log(error));
 
+
+
+////---------------------
+
+
+   async function loadFeatures(geojsonItem, map) {
+
+            return new Promise(function (resolve, reject) {
+                var features = new ol.format.GeoJSON().readFeatures(
+                    geojsonItem,
+                    { featureProjection: map.getView().getProjection() })
+                console.log(features)
+                resolve(features);
+            });
+        };
+
+
+
+
+        //2. Create an async function
+        async function executeLoadFeatureFunction() {
+            console.log('Before promise call.')
+            //3. Await for the first function to complete
+            const result = await loadFeatures(geojsonItem, map);
+            console.log(result);
+            console.log('Promise resolved: ' + result.toString())
+            console.log('Next step.')
+            return result
+        };
+
+
+        executeLoadFeatureFunction().then(....
+                                          )
