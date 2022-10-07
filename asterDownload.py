a328ed3f2e3c22ba7d79e1c60a481390b8214826
celikn <naimecelik@gmail.com>
2022-10-07 21:43:00 +0300

    Update asterDownload.py
    Added some example multiprocessings

--- asterDownload.py
+++ asterDownload.py
@@ -9,7 +9,7 @@ from requests.auth import HTTPDigestAuth
 
 
 ### BBOX içinde tanımlanmış alan içinde düşen raster verileri listeliyoruz.
-req = requests.get('https://cmr.earthdata.nasa.gov/search/granules.json?short_name=ASTGTM&version=003&page_size=2000&pageNum=1&bounding_box=23,31,46,44',auth=HTTPDigestAuth(username, password)).json()['feed']['entry'] 
+req = requests.get('https://cmr.earthdata.nasa.gov/search/granules.json?short_name=ASTGTM&version=003&page_size=2000&pageNum=1&bounding_box=29,31,30,44',auth=HTTPDigestAuth(username, password)).json()['feed']['entry'] 
 fileList = [g['links'][0]['href'] for g in req]
 print (fileList)
 
@@ -36,7 +36,7 @@ request.install_opener(opener)
 # caught and handled.
 
 
-## Verilen url'deki dosyayı indirmemizi sağlayan fonksiyon 
+## Verilen url'deki dosyayı indirmemizi sağlayan fonksiyon
 def dowloadfile(url):
         myrequest = request.Request(url)
         response = request.urlopen(myrequest)
@@ -57,7 +57,64 @@ def dowloadfile(url):
         return local_filename
 
 
-### BBOX ile elde ettiğimiz listeyi dönüyoruz.
-for urlItem in fileList:
-    dowloadfile(urlItem)
 
+def main():
+       
+        parallelRun=3 
+
+        
+        if parallelRun == 0: ## No parallel run 
+                ### BBOX ile elde ettiğimiz listeyi dönüyoruz.
+
+            for urlItem in fileList:
+                dowloadfile(urlItem)
+
+        elif parallelRun == 1: ## parallel with multiprocessing, main function'in  if __name__ == '__main__' ile cagirilmasi gerekli
+
+            import multiprocessing
+            from multiprocessing import cpu_count
+            with multiprocessing.Pool(cpu_count()) as pool:
+                # call the function for each item in parallel
+                pool.map(dowloadfile, fileList)
+
+        elif parallelRun == 2: ## Using asyncio 
+            import asyncio
+
+            def background(f):
+                def wrapped(*args, **kwargs):
+                    return asyncio.get_event_loop().run_in_executor(None, f, *args, **kwargs)
+
+                return wrapped
+
+            @background
+            def dowloadfileAsyncio(url):
+                myrequest = request.Request(url)
+                response = request.urlopen(myrequest)
+
+                # Check if status is OK
+                if response.code != 200:
+                    raise ValueError
+
+                response.begin()
+                local_filename = url.split('/')[-1]
+                with open(local_filename, 'wb') as f:
+                        while True:
+                            chunk = response.read()
+                            if chunk: 
+                                f.write(chunk)
+                            else:
+                                break
+                return local_filename
+
+            for urlItem in fileList:
+                dowloadfileAsyncio(urlItem)
+
+        elif parallelRun == 3: ## joblib !!! Not working with M1Chip
+            from joblib import Parallel, delayed
+            Parallel(n_jobs=2)(delayed(dowloadfile)(urlItem) for urlItem in fileList)
+
+        
+        
+
+if __name__ == '__main__':
+    main()
